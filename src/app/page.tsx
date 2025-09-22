92876048c6e83409bb12ac6d8276592f322e3b6c
rajveo23 <rajveo23@gmail.com>
2025-09-22 22:49:56 +0530

    Add files via upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Hero from '@/components/Hero'
-import Services from '@/components/Services'
-import WhyChooseUs from '@/components/WhyChooseUs'
-import TeamProfile from '@/components/TeamProfile'
-import ClientTestimonials from '@/components/ClientTestimonials'
-import ContactCTA from '@/components/ContactCTA'
+import Hero from '../components/Hero'
+import Services from '../components/Services'
+import WhyChooseUs from '../components/WhyChooseUs'
+import TeamProfile from '../components/TeamProfile'
+import ClientTestimonials from '../components/ClientTestimonials'
+import ContactCTA from '../components/ContactCTA'
 
 export default function Home() {
   return (
